742ad84f3094cc1db7ac1c5aaa7de89d59856b20
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2024-04-30 00:44:02 -0600

    it adds appliction properties configuration and data initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,20 @@
  * Created: Apr 29, 2024
  */
 
+INSERT INTO USER_ACCOUNT (id, username, name, last_name) VALUES (1,'userNameOne','Omar','Alvarez');
+INSERT INTO USER_ACCOUNT (id, username, name, last_name) VALUES (2,'userNameTwo','Jonny','Deep');
+INSERT INTO USER_ACCOUNT (id, username, name, last_name) VALUES (3,'userNameFour','Henry','Cavill');
+INSERT INTO USER_ACCOUNT (id, username, name, last_name) VALUES (4,'userNameThree','Brad','Pitt');
+
+
+
+
+
+INSERT INTO BOOK (id, title, author, available) VALUES (1,'Lord Of the Rings','J.R.R. Tolkien',true);
+INSERT INTO BOOK (id, title, author, available) VALUES (2,'Harry Potter','J.K Rowling',false);
+INSERT INTO BOOK (id, title, author, available) VALUES (3,'The Hobbit','J.R.R. Tolkien', true);
+INSERT INTO BOOK (id, title, author, available) VALUES (4,'Lean Startup','Eric Ries', true);
+INSERT INTO BOOK (id, title, author, available) VALUES (5,'Spring in Action','Craig Walls',true);
+INSERT INTO BOOK (id, title, author, available) VALUES (6,'Clean Code','Robert C. Martin',false);
+INSERT INTO BOOK (id, title, author, available) VALUES (7,'The Theory Of Everything','Steve Hawkins',true);
+INSERT INTO BOOK (id, title, author, available) VALUES (8,'Hunger Games','Suzane Collins',true);
